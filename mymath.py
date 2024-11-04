c68bdf924004c9c4a767484dbb75bcc0149d7022
kat040443 <122400976@umail.ucc.ie>
2024-11-04 09:59:16 +0000

    third commit - fixed code

--- mymath.py
+++ mymath.py
@@ -1,14 +1,15 @@
+# change a-b to a+b
 def add_three(a, b, c):
-    return a-b+c
-
+    return a+b+c
+# change b*c to b-c
 def subtract_three(a, b, c):
-    return a-b*c
-
+    return a-b-c
+#change a/b to a*b
 def multiply_three(a, b, c):
-    return a/b*c
-
-def divide_three(a, b, c):
     return a*b*c
-
+#change a*b*C to a/b/c
+def divide_three(a, b, c):
+    return a/b/c
+#change a*b to a**b
 def power(a, b):
-    return a*b
+    return a**b
