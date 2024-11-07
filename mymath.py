d6b8fe1bb55f3c38736e225e7107609ee7137a1b
kat040443 <122400976@umail.ucc.ie>
2024-11-07 21:21:18 +0000

    fourth commit - benefits of remote repository

--- mymath.py
+++ mymath.py
@@ -13,3 +13,16 @@ def divide_three(a, b, c):
 # change a*b to a**b
 def power(a, b):
     return a**b
+
+# Collaboration and Accessibility
+# All team members have access to the latest code and can synchronise changes automatically.
+# Branching and merging allows for isolated development without impacting the main code, supporting sade integration of
+# new features or fixes.
+
+# Version Control
+# Remote repositories provide a history of changes, enabling a return to previous versions when needed.
+# Team members ca retrieve older code versions or restore previously removed code, supporting better code management and accountability.
+
+# Redundancy and Backup
+# Remote repositories act as a backup, securing the code and ensuring it's accessible even if local copies are lost or compromised.
+# Devs can quickly resume work by re=cloning the repository.
